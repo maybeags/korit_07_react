b3aa0106a16a17222cf0bd86ab49ff5dabee2e08
maybeags <suren0709@gmail.com>
2025-10-15 16:12:40 +0900

    feat: event handler

--- mycontexapi/src/App.jsx
+++ mycontexapi/src/App.jsx
@@ -1,5 +1,9 @@
 import MyComponent from './MyComponent'
+import MyComponent2 from './MyComponent2'
 import AuthContext from './AuthContext'
+import MyList from './MyList'
+import MyTable from './MyTable'
+import MyForm from './MyFrom'
 import './App.css'
 
 function App() {
@@ -8,7 +12,10 @@ function App() {
 
   return (
     <AuthContext.Provider value={username}>
+      <MyForm />
       <MyComponent />
+      <MyTable />
+      <MyComponent2 />
     </AuthContext.Provider>
   )
 }
