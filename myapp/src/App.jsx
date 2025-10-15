ba9522968f1eb4d86a8e3a10fba738111be1f00c
maybeags <suren0709@gmail.com>
2025-10-15 10:39:11 +0900

    feat: useEffect()

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,11 +1,12 @@
-import Counter from './Counter';
+import Counter2 from './Counter2';
+import MyComponent3 from './MyComponent3';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Counter />
+      <Counter2 />
     </>
   );
 }
