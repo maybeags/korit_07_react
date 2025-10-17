330c196f28f86814ea5d20e6601163bc3df75d6a
maybeags <suren0709@gmail.com>
2025-10-17 12:38:56 +0900

    feat : Promise / fetch API / axios / OpenWeather API

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -21,11 +21,18 @@ function App() {
     .catch(error => console.log(error))
   }, []);
 
-  return (
-    <>
-
-    </>
-  );
+  if(weather.icon) {
+    return (
+      <>
+        <p>기온 : {weather.temp}</p>
+        <p>설명 : {weather.desc}</p>
+        <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="날씨 아이콘" />
+      </>
+    );
+  }
+  else {
+    return <div>Loading ...</div>
+  }
 }
 
 export default App
