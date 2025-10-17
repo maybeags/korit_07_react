7e24c922b61c5d1551a4e7bee5305dd117303572
maybeags <suren0709@gmail.com>
2025-10-17 11:30:47 +0900

    feat:api application

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [ weather, setWeather ] = useState({
+    temp: '',
+    desc: '',
+    icon: ''
+  });
   (
     <>
-      
+
     </>
   );
 }
