97528d582d8b1e20723741f268b11f16def87fa8
maybeags <suren0709@gmail.com>
2025-10-17 17:47:39 +0900

    feat: 20251017 fetchAPI/axios/React-query

--- gitapi/src/App.jsx
+++ gitapi/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Repositories from './Repositories'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 function App() {
 
   return (
     <>
-
+      <QueryClientProvider client={queryClient}>
+        <Repositories />
+      </QueryClientProvider>
     </>
   )
 }
