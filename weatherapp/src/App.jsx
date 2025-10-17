77d9b535e0cf96568b23ee3564c70af36e39300d
maybeags <suren0709@gmail.com>
2025-10-17 12:16:54 +0900

    feat: openweather

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
@@ -7,7 +7,21 @@ function App() {
     desc: '',
     icon: ''
   });
-  (
+
+  useEffect(() => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Busan&units=Metric&APIkey=08315439ef75a9358a09e997111e70c9')
+    .then(response => response.json())
+    .then(result => {
+      setWeather({
+        temp: result.main.temp,
+        desc: result.weather[0].description,
+        icon: result.weather[0].icon
+      })
+    })
+    .catch(error => console.log(error))
+  }, []);
+
+  return (
     <>
 
     </>
