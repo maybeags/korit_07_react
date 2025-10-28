b0363cd1c008eed3385402577d7583fa0862e45c
maybeags <suren0709@gmail.com>
2025-10-28 12:49:44 +0900

    feat: painful...

--- shoppinglist/src/main.tsx
+++ shoppinglist/src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const googleClientId = 
+const googleClientId = `${import.meta.env.VITE_GOOGLE_CLIENT_ID}`;
+
+if (!googleClientId) {
+  console.warn('Google Client ID가 설정되지 않았습니다.');
+}
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
